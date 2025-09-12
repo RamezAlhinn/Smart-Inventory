7e71e56424071e5413ef0868e65c82930a5b589f
Ramez Alhinn <ramez.hn5@gmail.com>
2025-09-12 20:12:11 +0200

    Refactor forecast_prophet function to simplify input DataFrame and update return values

--- packages/core/forecasting/prophet_model.py
+++ packages/core/forecasting/prophet_model.py
@@ -1,16 +1,15 @@
 import pandas as pd
-from neuralprophet import NeuralProphet as Prophet
+from neuralprophet import NeuralProphet
 
-def forecast_prophet(sales_df: pd.DataFrame, horizon=14):
+def forecast_prophet(df, horizon=14):
     """
-    sales_df: DataFrame with columns ["date", "qty_sold"]
-    horizon: days to forecast
+    Forecast using NeuralProphet.
+    df: must have columns ["ds", "y"]
+    horizon: number of future days to forecast
     """
-    df = sales_df.rename(columns={"date": "ds", "qty_sold": "y"})
-    model = Prophet(daily_seasonality=True, yearly_seasonality=True)
+    model = NeuralProphet()
     model.fit(df, freq="D")
-
-    future = model.make_future_dataframe(periods=horizon)
+    
+    future = model.make_future_dataframe(df, periods=horizon)
     forecast = model.predict(future)
-
-    return forecast[["ds", "yhat", "yhat_lower", "yhat_upper"]]
+    return forecast
