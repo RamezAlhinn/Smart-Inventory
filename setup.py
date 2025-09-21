4d73f54206444a661a612f5b2d6f1f53d87a0786
Ramez Alhinn <ramez.hn5@gmail.com>
2025-09-21 21:24:41 +0200

    fixes for the testing pipeline

--- setup.py
+++ setup.py
@@ -1,7 +1,16 @@
 from setuptools import setup, find_packages
 
+from setuptools import setup, find_packages
+
 setup(
-    name="smart_inventory",
-    version="0.1",
-    packages=find_packages(include=["packages", "packages.*"]),
+    name="smart-inventory",
+    version="0.2.0",
+    packages=find_packages(where="src"),  # look inside src/
+    package_dir={"": "src"},              # root is src/
+    install_requires=[
+        "pandas",
+        "prophet",
+        "streamlit",
+        # add other dependencies from requirements.txt if needed
+    ],
 )
